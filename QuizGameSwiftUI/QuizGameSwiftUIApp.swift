dcfaf46b2f319d170c9df65627639980e9b8596a
Dominik Hel <domhel00@gmail.com>
2023-01-04 12:28:30 +0100

    Initial Commit

---
+++ QuizGameSwiftUI/QuizGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameSwiftUIApp.swift
+//  QuizGameSwiftUI
+//
+//  Created by Dominik Hel on 04.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
