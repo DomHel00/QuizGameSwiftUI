e6ca2122584c405fb156b8299ac64d0509f42199
Dominik Hel <domhel00@gmail.com>
2023-01-16 19:42:07 +0100

    Adding QuizResult model.

--- QuizGameSwiftUI/Models/QuizResult.swift
+++ QuizGameSwiftUI/Models/QuizResult.swift
@@ -9,16 +9,19 @@
 import Foundation
 
 //  MARK: - Struct QuizResult
-struct QuizResult: Codable {
+struct QuizResult: Codable, Hashable {
     let numberOfQuestions: Int
     let numberOfCorrectAnswers: Int
     let numberOfIncorrectAnswers: Int
+    let quizCategory: String
+    var creationDate = Date().formatted(date: .abbreviated, time: .omitted)
     
-    /// Returns the success rate as a percentage.
-    var successPercentage: Double {
-        return Double(numberOfCorrectAnswers) / Double(numberOfQuestions)
+    /// Returns the percentage success rate.
+    var percentageSuccessRate: String {
+        let percentage = Double(numberOfCorrectAnswers) / Double(numberOfQuestions) * 100.0
+        return String(format:"%.2f", percentage)
     }
     
     // Only for preview.
-    static let example = QuizResult(numberOfQuestions: 10, numberOfCorrectAnswers: 5, numberOfIncorrectAnswers: 5)
+    static let example = QuizResult(numberOfQuestions: 10, numberOfCorrectAnswers: 5, numberOfIncorrectAnswers: 5, quizCategory: "Books")
 }
